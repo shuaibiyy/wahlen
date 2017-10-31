1f51283af2f2de99a30828904c417f8ece5ab1db
Shuaib Yunus <shuaibiyy@gmail.com>
2017-10-31 22:16:04 +0100

    Add bar charts

--- task1.py
+++ task1.py
@@ -1,8 +1,11 @@
 # coding=utf-8
 # Written using python 3.6
 import csv
-import pprint
+import pandas as pd
 from functools import reduce
+import matplotlib.pyplot as plt
+
+plt.rcdefaults()
 
 
 def fetch_rows(csv_path):
@@ -66,9 +69,7 @@ def aggregate(kv):
 def cleanse_votes(dirty):
     """Return a sorted list of parties with non-zero votes."""
     unsorted_votes = list(map(aggregate, map(filter_dashes, dirty)))
-
     non_zero_votes = list(filter(lambda x: x[1] != 0, unsorted_votes))
-
     return sorted(non_zero_votes, key=lambda tup: tup[1], reverse=True)
 
 
@@ -91,15 +92,69 @@ def votes():
     unfiltered_values = list(map(lambda x: (x, second_votes(x, vs)), ps))
 
     cleansed_votes = cleanse_votes(unfiltered_values)
-
     vote_total = reduce(lambda acc, kv: acc + kv[1], cleansed_votes, 0)
 
     return votes_with_percentages(vote_total, cleansed_votes)
 
 
-# For Printing #########
-print('Party;Percentage')
-for row in votes():
-    print('{0};{1}'.format(row[0], row[1]))
+def total_below(vs, percent):
+    """Return total votes below the provided percentage."""
+    other_votes = list(filter(lambda x: float(x[1]) < percent, vs))
+    return reduce(lambda acc, kv: acc + float(kv[1]), other_votes, 0)
 
-# pprint.pprint(votes())
+
+def chart():
+    """Display a bar chart of parties to the percentages of their votes."""
+    vs = votes()
+
+    above_five_percent = list(filter(lambda x: float(x[1]) >= 5, vs))
+    total_below_5_percent = total_below(vs, 5)
+    above_five_percent.extend([('others', total_below_5_percent)])
+
+    percents = list(map(lambda x: float(x[1]), above_five_percent))
+    party_titles = list(map(lambda x: x[0], above_five_percent))
+    x_pos = range(len(party_titles))
+
+    colors = 'rgbkymc'
+
+    plt.bar(x_pos, percents, align='center', color=colors, alpha=0.6)
+    plt.xticks(x_pos, party_titles)
+    plt.ylabel('Percentages of Votes Won')
+    plt.xlabel('Parties')
+    plt.title('2017 German Election Results')
+
+    plt.show()
+
+
+def chart_with_labels():
+    """Display a bar chart with percentage labels for each bar."""
+    vs = votes()
+
+    above_five_percent = list(filter(lambda x: float(x[1]) >= 5, vs))
+    total_below_5_percent = total_below(vs, 5)
+    above_five_percent.extend([('others', total_below_5_percent)])
+
+    percents = list(map(lambda x: float(x[1]), above_five_percent))
+    party_titles = list(map(lambda x: x[0], above_five_percent))
+
+    freq_series = pd.Series.from_array(percents)
+
+    plt.figure(figsize=(12, 8))
+    ax = freq_series.plot(kind='bar')
+    ax.set_title('2017 German Election Results')
+    ax.set_xlabel('Parties')
+    ax.set_ylabel('Percentages of Votes Won')
+    ax.set_xticklabels(party_titles)
+
+    bars = ax.patches
+
+    labels = ['{:.2f}'.format(percents[i]) + '%' for i in range(len(percents))]
+
+    for rect, label in zip(bars, labels):
+        height = rect.get_height()
+        ax.text(rect.get_x() + rect.get_width() / 2, height, label, ha='center', va='bottom')
+
+    plt.show()
+
+
+chart_with_labels()
