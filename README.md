5c79eb0e537d7fa0241bd461fd3e957b53a84283
Shuaib Yunus <shuaibiyy@gmail.com>
2017-12-07 23:29:53 +0100

    Support final bundestag seat distribution

--- README.md
+++ README.md
@@ -2,4 +2,32 @@
 
 Learning python by exploring Germany's 2017 election results.
 
+## Requirements
+
+* [pandas](https://pandas.pydata.org/pandas-docs/stable/install.html)
+
+## Usage
+
+The python script produces the output of some of the steps involved in the allocation of seats in the Bundestag.
+Available output:
+
+__Second votes (Zweitstimmen)__:
+```
+python wahl.py second_votes # generates _second_votes.csv
+
+python wahl.py second_votes_chart # opens up a bar chart in a new window
+```
+
+__Direct seats and list seats (Direktmandat, Listenmandat and Überhang)__:
+```
+python wahl.py direct_list_seats # generates _direct_list_seats.csv
+```
+
+__Final Bundestag Seat Distribution__:
+```
+python wahl.py bundestag_seats # generates _bundestag_seats.csv
+```
+
+## Tests
+
 Tests are written using [doctest](https://docs.python.org/3/library/doctest.html).
--- wahl.py
+++ wahl.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 # Written using python 3.6
+import sys
 import csv
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -33,7 +34,9 @@ def get_csv_values(csv_path):
 
 
 CSV_VOTES = get_csv_values('./ergebnisse.csv')
+
 CSV_POPULATION = get_csv_values('./population.csv')
+
 ALTERNATE_NAMES = [
     ['DIE LINKE', ['DIE LINKE.']],
     ['GRÜNE', ['GRÜNE/B 90', 'EB: Gruner']],
@@ -396,11 +399,12 @@ def chart_with_labels():
     plt.show()
 
 
-def display_votes():
+def write_second_votes():
     """Display semicolon separated values of parties and their vote percentages."""
-    print('Party;Votes')
-    for row in second_votes():
-        print('{0};{1}'.format(row[0], row[1]))
+    with open('_second_votes.csv', 'w') as csv_file:
+        csv_file.write('Party;Votes\n')
+        for v in second_votes():
+            csv_file.write('{0};{1}\n'.format(v[0], v[1]))
 
 
 def compute_seat_distribution(total_seats, proportions):
@@ -459,26 +463,29 @@ def second_vote_seat_distribution():
         map(lambda x: (x[0], compute_state_seats(x, state_distribution, parties)), votes_by_state))
 
 
-def display_seat_distribution():
+def write_direct_and_list_seats():
     """Print the seat distribution in the Bundestag."""
     first = states_direct_seats()
     second = second_vote_seat_distribution()
 
-    print('state;party;direct_seats;list_seats;ueberhang')
-    for i in second:
-        for j in first:
-            # if it's the same state
-            if i[0] == j[0]:
-                for k in i[1]:
-                    first_vote_counterpart = lookup_1st_value(j[1], k[0])
-                    state_name = lookup_state_name(i[0])
+    with open('_direct_list_seats.csv', 'w') as csv_file:
+        csv_file.write('state;party;direct_seats;list_seats;ueberhang\n')
+        for i in second:
+            for j in first:
+                # if it's the same state
+                if i[0] == j[0]:
+                    for k in i[1]:
+                        first_vote_counterpart = lookup_1st_value(j[1], k[0])
+                        state_name = lookup_state_name(i[0])
 
-                    # Ueberhang is the difference when direct seats are more than list seats.
-                    ueberhang = int(first_vote_counterpart[1]) - int(k[1])
-                    if ueberhang < 0:
-                        ueberhang = 0
+                        # Ueberhang is the difference when direct seats are more than list seats.
+                        ueberhang = int(first_vote_counterpart[1]) - int(k[1])
+                        if ueberhang < 0:
+                            ueberhang = 0
 
-                    print('{0};{1};{2};{3};{4}'.format(state_name, k[0], first_vote_counterpart[1], k[1], ueberhang))
+                        csv_file.write(
+                            '{0};{1};{2};{3};{4}\n'.format(state_name, k[0], first_vote_counterpart[1], k[1],
+                                                           ueberhang))
 
 
 def sum_party_across_states(values, party):
@@ -619,26 +626,125 @@ def federal_seat_distribution():
     return compute_mindessitzzahl_distribution(total_seats, eligible_votes, federal_mindessitz)
 
 
-def party_seat_distribution(federal_seats, party, votes):
-    """Distribute the seats for a party in its states."""
+def resolve_direkt_mandat_minimum(direkt_mandat_seats, state_seat, party):
+    """Each state list must receive at least as many seats as the number
+     of constituencies which the party won in the Land in question.
+     >>> resolve_direkt_mandat_minimum([('2', [('SPD', 5), ('CDU', 1)])], ('2', 4), 'SPD')
+     ('2', 5)
+     >>> resolve_direkt_mandat_minimum([('4', [('SPD', 7), ('CDU', 6)])], ('4', 4), 'CDU')
+     ('4', 6)
+     """
+    _, direkt_seat = lookup_party_in_state(direkt_mandat_seats, state_seat[0], party)
+
+    if direkt_seat > state_seat[1]:
+        return state_seat[0], direkt_seat
+
+    return state_seat
+
+
+def party_seat_distribution(direct_seats, federal_seats, party, votes):
+    """Distribute the seats for a party in its states potentially going over the allocated number of seats."""
     party_across_states = lookup_party_across_states(votes, party)
     cleansed_states = [x for x in party_across_states if len(x[1]) != 0]
     party_votes = [('{0}__{1}'.format(x[0], x[1][0]), x[1][1]) for x in cleansed_states]
     _, total_seats = lookup_1st_value(federal_seats, party)
 
-    distribution = compute_seat_distribution(total_seats, party_votes)
+    distribution = [(x[0].split('__')[0], x[1]) for x in compute_seat_distribution(total_seats, party_votes)]
 
-    return [(lookup_state_name(x[0].split('__')[0]), x[1]) for x in distribution]
+    return [resolve_direkt_mandat_minimum(direct_seats, x, party) for x in distribution]
+
+
+def second_smallest(numbers):
+    """Find the second smallest number. Lifted from Stack Overflow.
+    >>> second_smallest([2, 32, -0.23, 0.11, 1])
+    0.11
+    >>> second_smallest([31, 17, 17])
+    17
+    >>> second_smallest([])
+    inf
+    """
+    m1, m2 = float('inf'), float('inf')
+    for x in numbers:
+        if x <= m1:
+            m1, m2 = x, m1
+        elif x < m2:
+            m2 = x
+    return m2
+
+
+def custom_round(x, base=10):
+    """Custom rounding function."""
+    return int(base * round(float(x) / base))
+
+
+def correct_over_allocation(direct_seats, federal_seats, party, votes, party_list_seats):
+    """Correct over-allocation of a party's seats in its states."""
+    party_seats = lookup_1st_value(federal_seats, party)[1]
+    _, total_votes = sum_party_across_states(votes, party)
+    divisor = total_votes / party_seats
+    lookup_votes = lookup_party_in_state
+
+    while True:
+        # candidate seats are derived from dividing the second votes of each state by the starting divisor.
+        initial_candidates = [(x[0], round(lookup_votes(votes, x[0], party)[1] / divisor)) for x in party_list_seats]
+
+        # no. of seats allocated to each state should be the max between its direct seats and list seats.
+        candidates_seats = [(x[0], max(x[1], lookup_votes(direct_seats, x[0], party)[1])) for x in initial_candidates]
+
+        if total(candidates_seats) != party_seats:
+            # 2 sets of divisor candidates are derived based on the previous candidate seats distribution.
+            # candidate divisors that would result in a divisor smaller than the previous divisor are discarded.
+            divisor_candidates_1 = [(lookup_votes(votes, x[0], party)[1] / (x[1] - 0.5)) for x in candidates_seats
+                                    if lookup_votes(votes, x[0], party)[1] > divisor]
+            divisor_candidates_2 = [(lookup_votes(votes, x[0], party)[1] / (x[1] - 1.5)) for x in candidates_seats
+                                    if lookup_votes(votes, x[0], party)[1] > divisor]
+
+            divisor_candidates = [x for x in divisor_candidates_1 if x > divisor] + \
+                                 [x for x in divisor_candidates_2 if x > divisor]
+
+            divisor_candidate_1 = min(divisor_candidates)
+            divisor_candidate_2 = second_smallest(divisor_candidates)
+            diff = divisor_candidate_2 - divisor_candidate_1
+
+            divisor = custom_round(divisor_candidate_2 - (diff / 2), (diff / 2))
+            continue
+
+        return [(lookup_state_name(x[0]), x[1]) for x in candidates_seats]
 
 
 def parties_seat_distributions():
-    """Distribute the seats for each party in its states."""
+    """Distribute the seats for each party in its states and correct over-allocations."""
     votes_by_state = second_votes_by_states()
     federal_seats = federal_seat_distribution()
     parties = [x[0] for x in federal_seats]
+    direct_seats = states_direct_seats()
 
-    return [(x, party_seat_distribution(federal_seats, x, votes_by_state)) for x in parties]
+    initial_distribution = [(x, party_seat_distribution(direct_seats, federal_seats, x, votes_by_state))
+                            for x in parties]
+    return [(x[0], correct_over_allocation(direct_seats, federal_seats, x[0], votes_by_state, x[1]))
+            for x in initial_distribution]
 
+
+def write_parties_seat_distributions():
+    with open('_bundestag_seats.csv', 'w') as csv_file:
+        csv_file.write('Party;State;Seats\n')
+        for p in parties_seat_distributions():
+            for s in p[1]:
+                csv_file.write('{0};{1};{2}\n'.format(p[0], s[0], s[1]))
+
+
+funcs = {
+    'bundestag_seats': write_parties_seat_distributions,
+    'direct_list_seats': write_direct_and_list_seats,
+    'second_votes': write_second_votes,
+    'second_votes_chart': chart_with_labels
+}
+
+if len(sys.argv) > 1:
+    if sys.argv[1] in funcs:
+        funcs[sys.argv[1]]()
+    else:
+        print("Output %s not supported" % sys.argv[1])
 
 if __name__ == '__main__':
     import doctest
